19f28db6bd255ed3dbf672e4580c153323a51ea1
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-07-31 13:29:46 -0300

    alterações da aula 4

--- script.js
+++ script.js
@@ -10,10 +10,28 @@ pizzaJson.map(function(item, index) {
     //pega as informações que estão dentro da div .pizza-item
     let pizzaItem = select('.models .pizza-item').cloneNode(true);
 
+    //adiciona a imagem
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
+
+    //adiciona o preco
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    //adiciona o nome
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+
+    //adiciona a descricao
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    //configura o evento de click
+    pizzaItem.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault();
+
+        select('.pizzaWindowArea').style.opacity = 0;
+        select('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            select('.pizzaWindowArea').style.opacity = 1;
+        }, 200)
+    })
 
     //clona os itens de .pizza-item e insere dentro de .pizza-area
     select('.pizza-area').append(pizzaItem);
