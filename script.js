0bd37302173c2f46f45f37de72a76ebe0d2ac26e
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-07-31 12:25:12 -0300

    alterações da aula 3

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ pizzaJson.map(function(item, index) {
     //pega as informações que estão dentro da div .pizza-item
     let pizzaItem = select('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     //clona os itens de .pizza-item e insere dentro de .pizza-area
     select('.pizza-area').append(pizzaItem);
 })
