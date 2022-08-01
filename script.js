53d304eade25dc31a3ce2d5c5444bae3ab4a83e9
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-08-01 11:37:58 -0300

    alterações aula 6

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const selectAll = (element) => {
     return document.querySelectorAll(element);
 }
 
+let modalQntd = 1;
+
 pizzaJson.map(function(item, index) {
     //pega as informações que estão dentro da div .pizza-item
     let pizzaItem = select('.models .pizza-item').cloneNode(true);
@@ -24,11 +26,12 @@ pizzaJson.map(function(item, index) {
     //adiciona a descricao
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
-    //configura o evento de click
+    //configurando a janela de detalhes da pizza
     pizzaItem.querySelector('a').addEventListener('click', (event) => {
         event.preventDefault();
 
         let key = event.target.closest('.pizza-item').getAttribute('data-key');
+        modalQntd = 1;
 
         //conferir as informacoes da pizza selecionada
         console.log(pizzaJson[key]);
@@ -38,6 +41,29 @@ pizzaJson.map(function(item, index) {
         select('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         select('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         select('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        select('.pizzaInfo--size.selected').classList.remove('selected');
+
+        console.log(selectAll('.pizzaInfo--size'));
+        
+        // retorna um array com as divs que possuem essa class
+        // executa um laço para cada item desse array
+        selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+
+            // 1- seleciona a tag span
+            // 2- pega o index da pizza no objeto
+            // 3- utiliza o index do query selector no array de sizes do objeto para obter o valor equivalente ao tamanho
+            // 4- adiciona isso ao html
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+
+        })
+
+        select('.pizzaInfo--qt').innerHTML = modalQntd;
 
         //animacao de abrir a janela
         select('.pizzaWindowArea').style.opacity = 0;
